f11c2c18374a7edcf8501025acd233d4932f4291
peternikitakis <peternikitakis@gmail.com>
2023-11-17 09:37:21 -0500

    s&p500 data added

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import pandas as pd 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import numpy as np
 
-dow1 = pd.read_csv('dowmtm.csv')
+#dow = pd.read_csv('dow.csv')
+sp = pd.read_csv('sp.csv')
 
-dow1["Growth"]=dow1["Close*"].pct_change() 
-print(dow1)
+#pd.concat([dow, sp], ignore_index=True)
 
+# Convert 'Close*' column to numeric, handling errors by coercing non-numeric values to NaN
+#dow["Growth"]=np.round(dow["Close*"].pct_change()*100,2)
+#dow=dow.dropna()
+sp['Close*'] = pd.to_numeric(sp['Close*'], errors='coerce')
+sp["Growth"]=np.round(sp["Close*"].pct_change()*100,2)
+sp['Date'] = pd.to_datetime(sp['Date'])
+sp=sp.dropna()
+
+#Create the figures and title
 plt.figure(figsize=(24,12))
-plt.title("The Dow Jones IA Stock Index Month-Month")
+plt.title("Dow Jones vs. S&P 500 Indexes")
 
-plt.plot(dow1['Date'], dow1['Growth'])
+#Plot 'Date' against 'Close*' for growth visualization
+#plt.plot(dow.Date, dow['Close*'])
+plt.plot(sp['Date'], sp['Close*'])
+
+print(sp)
 plt.show()
-
-plt.legend()
-
-
